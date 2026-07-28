1cca8d30152df5aeb33dc995a4bd99300d7c5ca8
DEV S?? <snakerr77@gmail.com>
2026-07-28 14:04:16 -0300

    Delete src directory

--- src/data/profile.ts
+++
@@ -1,9 +0,0 @@
-export const profile = {
-  name: 'Matheus Vinicius',
-  brand: 'SN',
-  role: 'Desenvolvedor Full Stack',
-  location: 'Brasil',
-  github: 'https://github.com/snakerr77-spec',
-  linkedin: '',
-  email: '',
-}
